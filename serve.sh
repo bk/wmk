b88f4cbc04c94a9653e6205fc221bc55c14b060f
Baldur Kristinsson <bk@mbl.is>
2021-11-10 10:33:51 +0000

    rename to wmk.py; remove obsolete files

--- serve.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-htdir="$(dirname $0)/htdocs"
-
-python3 -m http.server 7007 --bind 127.0.0.1 --directory $htdir
--- watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. venv/bin/activate
-
-while true; do
-    ./process_pages.py
-    inotifywait -e create -e move -e delete -e modify -r assets data content static templates
-    sleep 1
-done
